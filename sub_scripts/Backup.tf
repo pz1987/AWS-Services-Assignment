88c53b2f30ea626d9135916a911a3e75ca0f8940
pz1987 <118959076+pz1987@users.noreply.github.com>
2023-03-28 03:15:00 +0800

    Update Backup.tf

--- sub_scripts/Backup.tf
+++ sub_scripts/Backup.tf
@@ -1,14 +1,17 @@
+#Create AWS Backup Vault
+resource "aws_backup_vault" "my_backup_vault" {
+  name        = "My_Backup_Vault"
+  #kms_key_arn = aws_kms_key.example.arn
+}
+
 # Create an AWS Backup
 resource "aws_backup_plan" "my_backup_plan" {
   name = "My_Backup_Plan"
   rule {
     rule_name         = "My_Backup_Rule"
-    target_vault_name = "My_Backup_Vault"
+    target_vault_name = aws_backup_vault.my_backup_vault.name
     schedule          = "cron(0 10 * * ? *)"
-    #schedule  {
-    #  frequency  = "daily"
-    #  start_time = "10:00"
-    #}
+
     lifecycle {
       cold_storage_after = "90"
       delete_after       = "365"
